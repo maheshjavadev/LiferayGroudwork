86207757d8f3dafbbd0c007750a20aa6ca6afbc0
Prakash <prakash.inganalli@hpe.com>
2018-11-18 18:46:10 +0530

    First commit

---
+++ LiferayWorkspace/modules/ExampleLiferayMVCWeb/src/main/java/com/mahesh/liferaymvcdxp/tutorial/constants/ExampleLiferayMVCWebPortletKeys.java
@@ -0,0 +1,10 @@
+package com.mahesh.liferaymvcdxp.tutorial.constants;
+
+/**
+ * @author Prakash
+ */
+public class ExampleLiferayMVCWebPortletKeys {
+
+	public static final String ExampleLiferayMVCWeb = "ExampleLiferayMVCWeb";
+
+}
---
+++ LiferayWorkspace/modules/guestbook-web/src/main/java/com/mahesh/docs/guestbook/constants/GuestbookPortletKeys.java
@@ -0,0 +1,10 @@
+package com.mahesh.docs.guestbook.constants;
+
+/**
+ * @author Prakash
+ */
+public class GuestbookPortletKeys {
+
+	public static final String Guestbook = "com_mahesh_docs_guestbook_portlet_GuestbookPortlet";
+
+}
